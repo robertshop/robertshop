6d6dc0ddcfa487804a02e8ddb94bf2eb81c4fd04
robertshop <shoprobert24@gmail.com>
2024-10-24 15:00:30 +0530

    added goolge analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://robertshops.com',
+    integrations: [
+        partytown({
+            config: {
+              forward: ["dataLayer.push"],
+            },
+        }),
+    ],
+});
